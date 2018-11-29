19df9b972a8cb8f5cea820dc9a2d3f8d2945cedf
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2018-11-28 18:17:15 -0600

    Adding tensor support to the numeric and generic operations
    - move generic_operation primitive into arithmetics plugin

--- phylanx/plugins/matrixops/matrixops.hpp
+++ phylanx/plugins/matrixops/matrixops.hpp
@@ -18,7 +18,6 @@
 #include <phylanx/plugins/matrixops/dot_operation.hpp>
 #include <phylanx/plugins/matrixops/expand_dims.hpp>
 #include <phylanx/plugins/matrixops/extract_shape.hpp>
-#include <phylanx/plugins/matrixops/generic_operation.hpp>
 #include <phylanx/plugins/matrixops/gradient_operation.hpp>
 #include <phylanx/plugins/matrixops/hstack_operation.hpp>
 #include <phylanx/plugins/matrixops/identity.hpp>
